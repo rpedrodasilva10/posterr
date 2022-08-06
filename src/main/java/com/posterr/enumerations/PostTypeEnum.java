394f362a09639a763dd74c963635db099e77c7f6
Renan Silva <renan.silva@ext.hdi.com.br>
2022-08-06 19:29:36 -0300

    fix(post-type): add enum to define post type

---
+++ src/main/java/com/posterr/enumerations/PostTypeEnum.java
@@ -0,0 +1,12 @@
+package com.posterr.enumerations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PostTypeEnum {
+    ORIGINAL,
+    REPOST,
+    QUOTE
+}
